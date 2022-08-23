650e02a8f86d44f63de2d05c105545f8cdfec0a8
robiniarockiadoss <106137236+robiniarockiadoss@users.noreply.github.com>
2022-08-23 14:18:22 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,27 @@
+const a = 30;
+const b = 20;
+
+
+const c = a+b;
+const g = a-b;
+const h = a*b;
+const i = a/b;
+const j = a^b;
+const k = a&b;
+
+
+
+console.log(c);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+//another method
+console.log('Value of A = ${a}');
+console.log('Value of B = ${b}');
+console.log("MATHEMATICAL EXPRESSIONS");
+console.log("Add the value A and B is ",(a+b));
+console.log("Subtract the value A and B is ",(a-b));
+console.log("Multiply the value A and B is ",(a*b));
+console.log("Divide the value A and B is ",(a/b));
